dd30dbb19b2e21f6508f11a3150d2e4064aab106
Can Cekic <ac.cekic@outlook.com>
2024-02-03 13:00:17 +0100

    Cleared: dictionary declartion and definition

--- dictionary.h
+++ dictionary.h
@@ -1,37 +1,3 @@
-namespace workbench {
-	template <class Key, class Value>
-	struct KeyValuePair {
-	private:
-		const Key* keyvar;
-		const Value* valuevar;
+#pragma once
 
-	public:
-		KeyValuePair(const Key&, const Value&);
-		~KeyValuePair();
-
-		Key* key();
-		Value* value();
-	};
-
-	template <class Key, class Value, bool uniquevar = false>
-	class Dictionary {
-	private:
-		static const int initialCapacity = 16;
-
-		const KeyValuePair<Key, Value>* entries;
-		int capacity;
-		int length = 0;
-
-		bool unique(const Key&);
-
-	public:
-		Dictionary();
-		Dictionary(const int);
-		~Dictionary();
-		
-		void add(const Key&, const Value&);
-		void add(const KeyValuePair<Key, Value>&);
-		KeyValuePair<Key, Value>* get(const Key&);
-		int size();
-	};
-}
+// TODO Develop Dictionary declaration
