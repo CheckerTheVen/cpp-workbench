3071dadb95e7a6b49440e7497460bc8e13aa5a3c
Can Cekic <ac.cekic@outlook.com>
2024-02-07 14:07:45 +0100

    Added: Dictionary class definition

--- dictionary.cpp
+++ dictionary.cpp
@@ -11,6 +11,43 @@ namespace workbench {
 		Key key();
 		Value value();
 	};
+	
+	template <class Key, class Value, bool multiple = false, bool extendable = true, bool removable = false>
+	class Dictionary {
+	private:
+		KeyValuePair<Key, Value>&* entries;
+		static constexpr int initial = 16;
+		static constexpr int maximum = 65536;
+		int length;
+		int capacity;
+
+		void insert(const int, const KeyValuePair<Key, Value>&);
+		void extend();
+
+	public:
+		Dictionary();
+		Dictionary(const int, bool, bool, bool);
+
+		void add(const KeyValuePair<Key, Value>&);
+		void add(const Key&, const Value&);
+		void replace(const Key&, const KeyValuePair<Key, Value>&);
+		void replace(const Key&, const Key&, const Value&);
+		void replace(const int, const KeyValuePair<Key, Value>&);
+		void replace(const int, const Key&, const Value&);
+		void remove(const Key&);
+		void remove(const int);
+
+		KeyValuePair<Key, Value>& get(const Key&);
+		KeyValuePair<Key, Value>& get(const int);
+		bool exists(const Key&);
+		bool exists(const int);
+		int indexOf(const Key&);
+		int indexOf(const int);
+
+	};
+
+	template <typename Key, typename Value>
+	using IndexableDictionary = Dictionary<Key, Value, false, false>;
 }
 
 template <class Key, class Value>
