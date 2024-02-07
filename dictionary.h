cccbf1b55baa10c563977bc486b3264945c9524d
Can Cekic <ac.cekic@outlook.com>
2024-02-07 08:57:13 +0100

    Added: KeyValuePair class declaration

--- dictionary.h
+++ dictionary.h
@@ -1,3 +1,14 @@
 #pragma once
 
-// TODO Develop Dictionary declaration
+template <class Key, class Value>
+class KeyValuePair {
+private:
+	Key keyvar;
+	Value valuevar;
+
+public:
+	KeyValuePair(const Key&, const Value&);
+
+	Key key();
+	Value value();
+};
